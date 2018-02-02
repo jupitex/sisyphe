36c2bc4656ce718374855464443139c814e9fb57
labroche <corentin.labroche@inist.fr>
2018-02-02 12:19:58 +0100

    try to fix travis

--- test/Manufactory-test.js
+++ test/Manufactory-test.js
@@ -13,7 +13,7 @@ const path = require("path");
 const Task = require("../src/task");
 
 describe(`${pkg.name}/src/Manufactory.js`, function() {
-  this.timeout(20000);
+  this.timeout(40000);
   describe("#init", function() {
     it("should initialize", async function() {
       const manufactory = await new Manufactory({ workers: ["dumbWorker"] });
